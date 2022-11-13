2ee588b433ad049e84eeb27fa3e975ef00a58488
Warren Snipes <contact@warrensnipes.dev>
2022-11-13 01:23:26 -0500

    implemented custom resolvers for Review struct

--- model/models.go
+++ model/models.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
+
 type Semester string
 
 var (
@@ -49,10 +56,186 @@ type Course struct {
 	Code string `json:"code,omitempty"`
 }
 
+type Tag string
+
+const (
+	TagToughGrader            Tag = "TOUGH_GRADER"
+	TagGetReadyToRead         Tag = "GET_READY_TO_READ"
+	TagParticipationMatters   Tag = "PARTICIPATION_MATTERS"
+	TagExtraCredit            Tag = "EXTRA_CREDIT"
+	TagGroupProjects          Tag = "GROUP_PROJECTS"
+	TagAmazingLectures        Tag = "AMAZING_LECTURES"
+	TagClearGradingCriteria   Tag = "CLEAR_GRADING_CRITERIA"
+	TagGivesGoodFeedback      Tag = "GIVES_GOOD_FEEDBACK"
+	TagInspirational          Tag = "INSPIRATIONAL"
+	TagLotsOfHomework         Tag = "LOTS_OF_HOMEWORK"
+	TagHilarious              Tag = "HILARIOUS"
+	TagBewareOfPopQuizzes     Tag = "BEWARE_OF_POP_QUIZZES"
+	TagSoManyPapers           Tag = "SO_MANY_PAPERS"
+	TagCaring                 Tag = "CARING"
+	TagRespected              Tag = "RESPECTED"
+	TagLectureHeavy           Tag = "LECTURE_HEAVY"
+	TagGradedByFewThings      Tag = "GRADED_BY_FEW_THINGS"
+	TagAccessibleOutsideClass Tag = "ACCESSIBLE_OUTSIDE_CLASS"
+	TagOnlineSavvy            Tag = "ONLINE_SAVVY"
+)
+
+func (t Tag) IsValid() bool {
+	switch t {
+	case TagToughGrader, TagGetReadyToRead, TagParticipationMatters, TagExtraCredit, TagGroupProjects, TagAmazingLectures, TagClearGradingCriteria, TagGivesGoodFeedback, TagInspirational, TagLotsOfHomework, TagHilarious, TagBewareOfPopQuizzes, TagSoManyPapers, TagCaring, TagRespected, TagLectureHeavy, TagGradedByFewThings, TagAccessibleOutsideClass, TagOnlineSavvy:
+		return true
+	}
+	return false
+}
+
+func (t Tag) String() string {
+	return string(t)
+}
+
+type Grade string
+
+const (
+	GradeAPlus      Grade = "A_PLUS"
+	GradeA          Grade = "A"
+	GradeAMinus     Grade = "A_MINUS"
+	GradeBPlus      Grade = "B_PLUS"
+	GradeB          Grade = "B"
+	GradeBMinus     Grade = "B_MINUS"
+	GradeCPlus      Grade = "C_PLUS"
+	GradeC          Grade = "C"
+	GradeCMinus     Grade = "C_MINUS"
+	GradeDPlus      Grade = "D_PLUS"
+	GradeD          Grade = "D"
+	GradeDMinus     Grade = "D_MINUS"
+	GradeFPlus      Grade = "F_PLUS"
+	GradeF          Grade = "F"
+	GradeFMinus     Grade = "F_MINUS"
+	GradeIncomplete Grade = "INCOMPLETE"
+	GradeWithdrawn  Grade = "WITHDRAWN"
+	GradeNotSure    Grade = "NOT_SURE"
+	GradeOther      Grade = "OTHER"
+)
+
+func (g Grade) IsValid() bool {
+	switch g {
+	case GradeAPlus, GradeA, GradeAMinus, GradeBPlus, GradeB, GradeBMinus, GradeCPlus, GradeC, GradeCMinus, GradeDPlus, GradeD, GradeDMinus, GradeFPlus, GradeF, GradeFMinus, GradeIncomplete, GradeWithdrawn, GradeNotSure, GradeOther:
+		return true
+	}
+	return false
+}
+
+func (g Grade) String() string {
+	return string(g)
+}
+
+func GetGradeByString(gradeString string) Grade {
+	switch gradeString {
+	case "A+":
+		return GradeAPlus
+	case "A":
+		return GradeA
+	case "A-":
+		return GradeAMinus
+
+	case "B+":
+		return GradeBPlus
+	case "B":
+		return GradeB
+	case "B-":
+		return GradeBMinus
+
+	case "C+":
+		return GradeCPlus
+	case "C":
+		return GradeC
+	case "C-":
+		return GradeCMinus
+
+	case "D+":
+		return GradeDPlus
+	case "D":
+		return GradeD
+	case "D-":
+		return GradeDMinus
+
+	case "F+":
+		return GradeFPlus
+	case "F":
+		return GradeF
+	case "F-":
+		return GradeFMinus
+
+	case "Incomplete":
+		return GradeIncomplete
+	case "Drop/Withdrawal":
+		return GradeWithdrawn
+	case "Not sure yet":
+		return GradeNotSure
+	default:
+		return GradeOther
+	}
+}
+
 type Review struct {
-	Quality    float64 `json:"qualityRating"`
-	Difficulty float64 `json:"difficultyRatingRounded"`
-	DateString string  `json:"date"`
-	RatingTags string  `json:"ratingTags"`
-	Grade      string  `json:"grade"`
+	Quality    float64   `json:"quality"`
+	Difficulty float64   `json:"difficulty"`
+	Date       time.Time `json:"date"`
+	Tags       []Tag     `json:"tags"`
+	Grade      Grade     `json:"grade"`
+}
+
+func GetTagByString(tagString string) (Tag, error) {
+	formattedTagString := strings.ReplaceAll(strings.ToUpper(tagString), " ", "_")
+	tag := Tag(formattedTagString)
+	if !tag.IsValid() {
+		return "", fmt.Errorf("%s is an invalid Tag", tagString)
+	}
+	return tag, nil
+}
+
+// RMPTimeConstant Refer to the time.Layout type for more info
+const RMPTimeConstant = "2006-01-02 15:04:05 -0700 MST"
+
+func (r Review) UnmarshalJSON(bytes []byte) (err error) {
+	var data map[string]any
+
+	if err = json.Unmarshal(bytes, &data); err != nil {
+		return err
+	}
+	r.Quality = data["qualityRating"].(float64)
+	r.Difficulty = data["difficultyRatingRounded"].(float64)
+
+	dateString := data["date"].(string)
+	t, err := time.Parse(RMPTimeConstant, dateString)
+	if err != nil {
+		return err
+	}
+
+	r.Date = t
+
+	tagsString := data["ratingTags"].(string)
+
+	if len(tagsString) > 0 {
+		split := strings.Split(tagsString, "--")
+
+		tags := make([]Tag, 0, len(split))
+
+		for _, elem := range split {
+			tag, err := GetTagByString(elem)
+			if err != nil {
+				return err
+			}
+			tags = append(tags, tag)
+		}
+		r.Tags = tags
+	}
+
+	gradeString := data["grade"].(string)
+	grade := GetGradeByString(gradeString)
+	if !grade.IsValid() {
+		return fmt.Errorf("%s is an invalid grade", gradeString)
+	}
+	r.Grade = grade
+
+	return nil
 }
